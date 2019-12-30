4e90f6cb265dbafe86021f84ae6ad3c8f3962c03
aynurajafarova <aynurafj@code.edu.az>
2019-12-30 20:13:30 +0400

    momentsSlider section created

--- assets/js/owlCarousel.js
+++ assets/js/owlCarousel.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".owl-carousel").owlCarousel({
+  $(".main-slider-photos").owlCarousel({
     items: 1,
     nav: true,
     loop: true,
@@ -7,7 +7,7 @@ $(document).ready(function() {
     autoplayTimeout: 3000,
     margin: 35,
     dots: true,
-    dotsEach:true,
+    dotsEach: true,
     animateOut: "fadeOut",
     smartSpeed: 1500,
     responsiveClass: true,
@@ -27,6 +27,36 @@ $(document).ready(function() {
       991: {
         items: 1,
         nav: true,
+        loop: true
+      }
+    }
+  });
+
+  // momentsSliderPhotos
+  $(".moments-slider-photos").owlCarousel({
+    items: 1,
+    nav: false,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    margin: 35,
+    dots: true,
+    dotsEach: true,
+    animateOut: "fadeOut",
+    smartSpeed: 1500,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      575: {
+        items: 2
+      },
+      767: {
+        items: 3
+      },
+      991: {
+        items: 1,
         loop: true
       }
     }
