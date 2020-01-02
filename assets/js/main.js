617e7ede3139073e35f7a7d4a6a0e7928aacb172
aynurajafarova <aynurafj@code.edu.az>
2020-01-02 23:29:24 +0400

    main.js updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,14 @@ $(document).ready(function() {
     const header = $("#headerArea");
     const headerHeight = header.innerHeight();
     const scrollTop = $(this).scrollTop();
+    const logoImg = $(".logo-img img");
 
     if (scrollTop >= headerHeight) {
       header.addClass("scrolled");
+      logoImg.attr("src", "./assets/images/logo/logo-sticky.svg");
     } else {
       header.removeClass("scrolled");
+      logoImg.attr("src", "./assets/images/logo/logo.svg");
     }
   });
 });
